59b057c420e3af453a72f4b2e77d804ee02a8408
ghkdrudals <aa01046044029@gmail.com>
2025-08-14 00:17:25 +0900

    ennd

---
+++ project6/src/App.js
@@ -0,0 +1,21 @@
+import {
+  BROWSEROUTER as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+       <Switch>
+        <Route path="/">
+
+        </Route>
+       </Switch>
+    </Router>
+  );
+}
+
+export default App;
