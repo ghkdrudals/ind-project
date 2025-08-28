f58c93dfda3cefb87d24d24c097ea1b32682fad3
황경민 <aa01046044029@gmail.com>
2025-08-28 15:12:05 +0900

    start

---
+++ project-master2/src/App.js
@@ -0,0 +1,21 @@
+import styled, { keyframes } from "styled-components";
+
+const Wrapper = styled.div`
+display:flex;
+`
+const Box = styled.div`
+height:200px;
+width:200px;
+`
+
+const animation = keyframes`
+
+`
+function App() {
+  return (
+    <Wrapper>
+    </Wrapper>
+  );
+}
+
+export default App;
