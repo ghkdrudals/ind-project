f9286d4511274fd2ae31bcb74fe53bf588ff43f0
황경민 <aa01046044029@gmail.com>
2025-09-15 23:30:34 +0900

    final

--- project-master5/src/App.tsx
+++ project-master5/src/App.tsx
@@ -3,7 +3,10 @@ import { useState } from "react";
 import React from "react";
 
 const Container = styled.div`
- background-color: ${(props) => props.theme.bgClor};
+  background-color: ${(props) => props.theme.bgClor};
+`
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
 `
 function App() {
   const[value, setValue] = useState("");
@@ -17,12 +20,13 @@ function App() {
     console.log("hello",value)
   }
   return (
-    <div onSubmit={onSubmit}>
+    <Container onSubmit={onSubmit}>
+      <H1>protected</H1>
       <form>
         <input value={value} onChange={onChange} type="text" placeholder="usename"></input>
         <button>login</button>
       </form>
-    </div>
+    </Container>
   );
 }
 
