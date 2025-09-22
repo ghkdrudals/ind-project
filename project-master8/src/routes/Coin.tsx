35748ac210b7177adbbea834b8289f596abf6ea9
황경민 <aa01046044029@gmail.com>
2025-09-22 23:19:13 +0900

    middle4

--- project-master8/src/routes/Coin.tsx
+++ project-master8/src/routes/Coin.tsx
@@ -1,13 +1,43 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useState } from "react";
+import { useParams,useLocation } from "react-router-dom";
+import styled from "styled-components";
 
 type RouterParams={
     coinId:string;
 }
 
+const Title = styled.h1`
+    font-size:48px;
+    color:${(props)=>props.theme.accentColor};`
+
+const Loader=styled.span`
+    text-align:center;
+    display:block;
+`
+
+const Header=styled.header`
+height:10vh;
+display:flex;
+justify-content:center;
+align-items:center;
+`
+
+const Continer=styled.div`
+    padding:0px 10px;
+    `
+
 function Coin(){
+    const[loading,setLoading]=useState(true);
     const {coinId} = useParams<RouterParams>();
-    return <h1>coin: {coinId}</h1>
+    const location=useLocation();
+    return <Continer>
+    <Header>
+        <Title>Coins{coinId}</Title>
+    </Header>
+    {loading ? (<Loader>loading...</Loader>
+    ):null}
+    </Continer>
+    
 }
 
 export default Coin;
